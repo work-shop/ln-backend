cf5e82f0b0b8eff73c1b6ea2c9e0bf69a9660139
Nic Schumann <nicolas_schumann@brown.edu>
2017-02-10 11:46:13 -0500

    [CDN Option] Added an General Setting to specify a base CDN URL.

--- themes/headless/library/classes/class_init_actions.php
+++ themes/headless/library/classes/class_init_actions.php
@@ -11,6 +11,7 @@ class WS_Init_Actions extends WS_Action_Set {
 		parent::__construct(
 			array(
 				'init' 					=> 'setup',
+                'admin_init'            => 'admin_setup',
 				'after_theme_setup'		=> array( 'remove_post_formats', 11, 0 ),
 				'login_head'			=> 'login_css',
 				'admin_head'			=> 'admin_css',
@@ -110,6 +111,40 @@ class WS_Init_Actions extends WS_Action_Set {
 
 
 	}
+
+
+    /**
+     * Admin setup registers additional settings on the global options page for us.
+     *
+     * TODO: Need to update the `register_setting` function to take an array in the third parameter – once we're able to update to 4.7.3
+     * That API is not available in 4.6.3
+     */
+    public function admin_setup() {
+        register_setting(
+            'general',
+            'cdn_url'
+        );
+
+        add_settings_field(
+            'cdn_url',
+            'CDN Address (URL)',
+            array( $this, 'render_settings_field' ),
+            'general',
+            'default',
+            array( 'cdn_url', get_option('cdn_url') )
+        );
+    }
+
+    /**
+     * Callback function to render the CDN URL field in the options.
+     *
+     * @param $args array the array of value arguments
+     *
+     */
+    public function render_settings_field( $args ) {
+        echo "<input aria-describedby='cdn-description' name='cdn_url' class='regular-text code' type='text' id='" . $args[0] . "' value='" . $args[1] . "'/>";
+        echo "<p id='cdn-description' class='description'>Input the url of the CDN to use with this site or leave this field blank to bypass the CDN.";
+    }
 
 	/* CUSTOM MENU LINK FOR ALL SETTINGS - WILL ONLY APPEAR FOR ADMIN */
 	public function all_settings_link() {
